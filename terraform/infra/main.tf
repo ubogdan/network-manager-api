e8a063d463a0e9d720eabcd82c6485b888c62855
Bogdan Ungureanu <ubogdan@gmail.com>
2024-01-17 12:01:04 +0200

    terraform: add download bucket

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -14,10 +14,26 @@ terraform {
   backend "s3" {
     encrypt = true
     region  = "eu-central-1"
-    key     = "infrastructure-${var.stack_env}"
+    key     = "nm-api-infra"
+    bucket = "ubogdan-terraform-state"
   }
 }
 
+## Software releases download bucket
+resource "aws_s3_bucket" "downloads" {
+  bucket = var.download_bucket
+}
+
+resource "aws_s3_bucket_public_access_block" "downloads" {
+  bucket = var.backup_bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+## Clients backup's bucket
 resource "aws_s3_bucket" "backup" {
   bucket = var.backup_bucket
 }
--- terraform/infra/variables.tf
+++ terraform/infra/variables.tf
@@ -1,8 +1,14 @@
 variable "stack_env" {
   type = string
+  default = "prod"
 }
 
 variable "backup_bucket" {
   type    = string
   default = "nm-backup"
+}
+
+variable "download_bucket" {
+  type = string
+  default = "nm-release-downloads"
 }
